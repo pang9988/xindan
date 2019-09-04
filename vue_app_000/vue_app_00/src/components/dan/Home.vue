6de81d3cc0ed3643781e0946c45b14e6cb2dd53d
pang9988 <pzy5021@163.com>
2019-09-04 08:30:49 +0800

    11

---
+++ vue_app_000/vue_app_00/src/components/dan/Home.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="ho-header">
+    <div class="ho-search">
+      <!-- 1.搜索框组件 -->
+      来了
+      <mt-tab-container class="page-tabbar-container" v-model="active">
+        <mt-tab-container-item id="message"></mt-tab-container-item>
+      </mt-tab-container>
+      <!--3:切换面板列表-->
+      <mt-tabbar class="ho-color" fixed v-model="active">
+        <mt-tab-item id="message" @click.native="changeState(0)">
+          <tabbaricon
+            :normalImage="require('../../assets/dan.png')"
+            :selectedImage="require('../../assets/dans.png')"
+            :focused="currentIndex[0].isSelect"
+          ></tabbaricon>新蛋
+        </mt-tab-item>
+
+        <mt-tab-item id="contact" @click.native="changeState(1)">
+          <tabbaricon
+            :normalImage="require('../../assets/fen.png')"
+            :selectedImage="require('../../assets/fens.png')"
+            :focused="currentIndex[1].isSelect"
+          ></tabbaricon>分类
+        </mt-tab-item>
+
+        <mt-tab-item id="me" @click.native="changeState(2)">
+          <tabbaricon
+            :normalImage="require('../../assets/gou.png')"
+            :selectedImage="require('../../assets/gous.png')"
+            :focused="currentIndex[2].isSelect"
+          ></tabbaricon>购物车
+        </mt-tab-item>
+        <mt-tab-item id="me" @click.native="changeState(3)">
+          <tabbaricon
+            :normalImage="require('../../assets/ren.png')"
+            :selectedImage="require('../../assets/rens.png')"
+            :focused="currentIndex[3].isSelect"
+          ></tabbaricon>我
+        </mt-tab-item>
+      </mt-tabbar>
+      <!-- 父面板 -->
+    </div>
+  </div>
+</template>
+<script>
+//引入切换面板
+import TabBarIcon from "./common/TabBaricon.vue";
+export default {
+  data() {
+    return {
+      active: "message",
+      //创建数组保存图片焦点状态
+      currentIndex: [
+        { isSelect: true },
+        { isSelect: false },
+        { isSelect: false },
+        { isSelect: false }
+      ]
+    };
+  },
+
+  components: {
+    tabbaricon: TabBarIcon
+  },
+  methods: {
+    changeState(n) {
+      //函数功能:将当前参数下标
+      //对应数组值修改true其它修改false
+      //1:创建循环,循环数组中内容
+      for (var i = 0; i < this.currentIndex.length; i++) {
+        //2:判断如果循环下标与n相等 20
+        if (n == i) {
+          //3:当前下标元素true
+          this.currentIndex[i].isSelect = true;
+        } else {
+          //4:其它元素修改false
+          this.currentIndex[i].isSelect = false;
+        }
+      }
+    }
+  }
+};
+</script>>
+<style>
+.ho-color .mint-tab-item-label {
+  color: #666 !important;
+}
+</style>
--- vue_app_000/vue_app_00/src/router.js
+++ vue_app_000/vue_app_00/src/router.js
@@ -1,23 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloContainer from "./components/HelloWorld.vue"
-import Home from "./components/weixin/Home.vue"
+import Home3 from "./components/weixin/Home3.vue"
 // 登录
 import Login from "./components/dan/common/Login.vue"
  //邮箱登录
  import Email from "./components/dan/common/Email.vue"
-// 验证
-// import Regsiter from "./components/jia/Regsiter.vue"
-// 忘记密码
-// import Forget from "./components/jia/Forget.vue"
+//全局组件
+import Home from "./components/dan/Home.vue"
 //购物车
 // import Cart from "./components/jia/Cart.vue"
 Vue.use(Router)
 export default new Router({
   routes: [
     // {path:'/Cart',component:Cart},
-    // {path:'/Forget',component:Forget},
-    // {path:'/Regsiter',component:Regsiter},
+    {path:'/Home3',component:Home3},
     {path:'/Email',component:Email},
     {path:'/Login',component:Login},
     {path:'/Home',component:Home},
